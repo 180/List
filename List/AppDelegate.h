4a6d1eb945ce713fb21d7ee8ed752999c0783be3
Maciej Stanik <M.Stanik@kainos.com>
2015-04-11 20:40:34 +0200

    Initial Commit

---
+++ List/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  List
+//
+//  Created by Maciej Stanik on 11/04/2015.
+//  Copyright (c) 2015 Maciej Stanik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ List/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  List
+//
+//  Created by Maciej Stanik on 11/04/2015.
+//  Copyright (c) 2015 Maciej Stanik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
